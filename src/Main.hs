604f85192532a54b823a31aacf58c342df1b9048
Jon Taylor <jont@cakesolutions.net>
2017-09-08 19:41:43 +0100

    Read and decode diamond.json

--- src/Main.hs
+++ src/Main.hs
@@ -6,15 +6,23 @@ import Data.Aeson
 import GHC.Generics
 import qualified Data.ByteString.Lazy.Char8 as BS
     
-data Person = Person {
-    name :: String,
-    age  :: Int
-} deriving (Generic, Show)    
+data Cut = Cut {
+    size :: Int,
+    value :: Int
+} deriving (Generic, Show)
 
-instance ToJSON Person where
-    toEncoding = genericToEncoding defaultOptions
+instance FromJSON Cut
 
-instance FromJSON Person
+data Gem = Gem {
+    cuts :: [Cut],
+    rawChunks :: [Int]
+} deriving (Generic, Show)
+
+instance FromJSON Gem
 
 main :: IO ()
-main = BS.putStrLn $ encode (Person {name = "Joe", age = 12})
+main = do
+    let diamondJson = "/Users/jontaylor/HomeProjects/BrilliantCutHaskell/src/diamond.json"
+    getJSON <- BS.readFile diamondJson
+    let maybeDiamond = decode getJSON :: Maybe Gem
+    putStrLn $ show maybeDiamond
