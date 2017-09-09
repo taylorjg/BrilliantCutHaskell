79fe68c7516f528c5648934268ab0b104c93e2b5
Jon Taylor <jont@cakesolutions.net>
2017-09-09 11:32:15 +0100

    Read and decode input.json

--- src/Main.hs
+++ src/Main.hs
@@ -1,28 +1,50 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric, OverloadedStrings #-}
 
 module Main where
 
 import Data.Aeson
+import Data.Aeson.Types
 import GHC.Generics
 import qualified Data.ByteString.Lazy.Char8 as BS
+import qualified Data.HashMap.Strict as HM
     
 data Cut = Cut {
     size :: Int,
     value :: Int
 } deriving (Generic, Show)
 
-instance FromJSON Cut
 
 data Gem = Gem {
     cuts :: [Cut],
     rawChunks :: [Int]
 } deriving (Generic, Show)
 
+data Input = Input {
+    gems :: [Gem]
+} deriving Show
+
+instance FromJSON Cut
+
 instance FromJSON Gem
+
+instance FromJSON Input where
+    parseJSON = withObject "Input" $ \obj ->
+        let
+            vs = HM.elems obj
+            gemParser = parseJSON :: Value -> Parser Gem
+            gemsParser = mapM gemParser vs
+        in
+            Input <$> gemsParser
 
 main :: IO ()
 main = do
+
     let diamondJson = "/Users/jontaylor/HomeProjects/BrilliantCutHaskell/src/diamond.json"
-    getJSON <- BS.readFile diamondJson
-    let maybeDiamond = decode getJSON :: Maybe Gem
+    bs1 <- BS.readFile diamondJson
+    let maybeDiamond = decode bs1 :: Maybe Gem
     putStrLn $ show maybeDiamond
+
+    let inputJson = "/Users/jontaylor/HomeProjects/BrilliantCutHaskell/src/input.json"
+    bs2 <- BS.readFile inputJson
+    let maybeInput = decode bs2 :: Maybe Input
+    putStrLn $ show maybeInput
