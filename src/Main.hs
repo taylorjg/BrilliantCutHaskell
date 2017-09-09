255c3c6338c2f0fce8085c39a16974c10387e350
Jon Taylor <jont@cakesolutions.net>
2017-09-09 13:36:03 +0100

    First working version

--- src/Main.hs
+++ src/Main.hs
@@ -35,9 +35,50 @@ instance FromJSON Input where
         in
             Input <$> gemsParser
 
+generateCombinationsOfCuts :: Int -> [Cut] -> [Cut] -> [[Cut]]
+generateCombinationsOfCuts chunkSize availableCuts actualCuts =
+    do
+        cut <- availableCuts
+        let chunkSize' = chunkSize - size cut
+        case chunkSize' of
+            _ | chunkSize' > 0 ->
+                let
+                    actualCuts' = cut:actualCuts
+                    rest = generateCombinationsOfCuts chunkSize' availableCuts actualCuts'
+                in actualCuts':rest
+            _ -> []
+
+calculateProfitForCombinationOfCuts :: Int -> [Cut] -> Int
+calculateProfitForCombinationOfCuts rawChunk cuts =
+    profit
+    where
+        vsum = sum $ map value cuts
+        ssum = sum $ map size cuts
+        waste = rawChunk - ssum
+        profit = vsum - waste
+        
+calculateMaxProfitForRawChunk :: [Cut] -> Int -> Int
+calculateMaxProfitForRawChunk availableCuts rawChunk =
+    v3
+    where
+        v1 = generateCombinationsOfCuts rawChunk availableCuts []
+        v2 = map (calculateProfitForCombinationOfCuts rawChunk) v1
+        v3 = maximum v2
+
+calculateMaxProfitsForRawChunks :: Gem -> [Int]
+calculateMaxProfitsForRawChunks gem =
+    map (calculateMaxProfitForRawChunk (cuts gem)) (rawChunks gem)
+
+largestProfit :: Input -> Int
+largestProfit = sum . map sum . map calculateMaxProfitsForRawChunks . gems
+
 main :: IO ()
 main = do
     let inputJson = "/Users/jontaylor/HomeProjects/BrilliantCutHaskell/src/input.json"
     bs <- BS.readFile inputJson
     let maybeInput = decode bs :: Maybe Input
-    putStrLn $ show maybeInput
+    case maybeInput of
+        Just input ->
+            let answer = largestProfit input
+            in putStrLn $ "Largest profit: " ++ show answer
+        _ -> putStrLn "Error reading input.json"
